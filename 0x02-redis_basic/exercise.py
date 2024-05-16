39fb4e3ae99d901d3a22257d17d888019113c9a6
trust <isaactrust447@gmail.com>
2024-05-16 04:00:04 +0100

    go

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,6 +22,52 @@ def count_calls(method: callable) -> callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """store the history of inputs and outputs"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper for the decorated function"""
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+    return wrapper
+
+
+def replay(fn: Callable):
+    """display the history of calls of a particular function"""
+    r = redis.Redis()
+    function_name = fn.__qualname__
+    value = r.get(function_name)
+    try:
+        value = int(value.decode("utf-8"))
+    except Exception:
+        value = 0
+
+    # print(f"{function_name} was called {value} times")
+    print("{} was called {} times:".format(function_name, value))
+    # inputs = r.lrange(f"{function_name}:inputs", 0, -1)
+    inputs = r.lrange("{}:inputs".format(function_name), 0, -1)
+
+    # outputs = r.lrange(f"{function_name}:outputs", 0, -1)
+    outputs = r.lrange("{}:outputs".format(function_name), 0, -1)
+
+    for input, output in zip(inputs, outputs):
+        try:
+            input = input.decode("utf-8")
+        except Exception:
+            input = ""
+
+        try:
+            output = output.decode("utf-8")
+        except Exception:
+            output = ""
+
+        # print(f"{function_name}(*{input}) -> {output}")
+        print("{}(*{}) -> {}".format(function_name, input, output))
+
+
 class Cache:
     """Cache class for interacting with Redis"""
 
@@ -30,6 +76,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls  
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in the cache and return the stored key
         letters = string.ascii_letters + string.digits
@@ -45,3 +93,4 @@ class Cache:
         if fn:
             val = fn(val)
         return val
+    def get_str(self, key: str) -> str:
