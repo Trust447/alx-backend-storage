b70214ff46fe3db895f3af34d0ad42cbc0b9ceb6
trust <isaactrust447@gmail.com>
2024-09-09 01:37:34 +0100

    go

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -17,23 +17,23 @@ def url_access_count(method):
         cache_key = "cached:" + url
         count_key = "count:" + url
 
-
         cached_value = r.get(cache_key)
         if cached_value:
             return cached_value.decode("utf-8")
-
-
+            
         html_content = method(url)
         r.incr(count_key)
         r.set(cache_key, html_content, ex=10)
         return html_content
     return wrapper
 
+
 @url_access_count
 def get_page(url: str) -> str:
     """Obtain the HTML content of a particular URL."""
     response = requests.get(url)
     return response.text
+
 
 if __name__ == "__main__":
 
