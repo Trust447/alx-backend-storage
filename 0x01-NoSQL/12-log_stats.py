f1f5a7cc0422fa6fce9851689d21e925f20725a4
trust <isaactrust447@gmail.com>
2024-10-02 16:52:57 +0100

    nosql

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,31 +9,28 @@ one line with method=GET, path=/status
 """
 from pymongo import MongoClient
 
-
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-
-def log_stats(mongo_collection, option=None):
+def log_stats(mongo_collection):
     """
-    Prototype: def log_stats(mongo_collection, option=None):
+    Prototype: def log_stats(mongo_collection):
     Provide some stats about Nginx logs stored in MongoDB
     """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
+    # Count total number of logs
+    total_logs = mongo_collection.count_documents({})
+    print(f"{total_logs} logs")
     print("Methods:")
+    
+    # Count logs by method
     for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
+        method_count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+    
+    # Count logs for status check
+    status_check = mongo_collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
 
-
 if __name__ == "__main__":
+    # Connect to the MongoDB server and access the collection
     nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
     log_stats(nginx_collection)
